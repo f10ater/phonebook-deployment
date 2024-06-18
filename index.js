be1d19940f28aeb681c62d9e3af2c0c717545c16
AmiteshwarRandhawa <amiteshwar04@gmail.com>
2024-06-19 00:32:10 +0530

    Add frontend and integrate with backend

--- index.js
+++ index.js
@@ -6,42 +6,44 @@ const app = express()
 morgan.token('body', (request) => JSON.stringify(request.body))
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
       id: 1,
       name: "Arto Hellas", 
-      number: "040-123456"
+      phone: "040-123456"
     },
     { 
       id: 2,
       name: "Ada Lovelace", 
-      number: "39-44-5323523"
+      phone: "39-44-5323523"
     },
     { 
       id: 3,
       name: "Dan Abramov", 
-      number: "12-43-234345"
+      phone: "12-43-234345"
     },
     { 
-      id: 4,
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
+        id: 4,
+        name: "Mary Poppendieck", 
+        phone: "39-23-6423122"
     }
 ]
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(body)
     const id = Math.floor(Math.random() * 999999)
 
-    if(!body.name || !body.number)
-        return response.status(400).json({ error: 'both name and number must be provided'})
+    if(!body.name || !body.phone)
+        return response.status(400).json({ error: 'both name and phone must be provided'})
 
     const person = persons.find(person =>  person.name === body.name )
     if(person)
         return response.status(400).json({error: 'person already in the data'})   
+
+    body.id = id
     
     persons = persons.concat(body)
     response.json(body)
@@ -60,9 +62,9 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
     response.status(204).end()
 })
 
@@ -70,6 +72,13 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p>
                     <p>${new Date()}`)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
 
 const PORT = process.env.PORT || 3001 
 
